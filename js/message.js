1c316cc69c2fb5ac02453186f1a24026190e861c
ggblockster <ggblockster@gmail.com>
2026-07-19 18:19:10 +1000

    Add files via upload

---
+++ js/message.js
@@ -0,0 +1,18 @@
+console.log("\n%cPokéGuessr v1.0\n", 
+    "color: lightgreen; font-size: 36px; font-family: Bahnschrift, sans-serif");
+
+console.log(`\n\t%cThis was made with plain HTML, CSS, and JavaScript by ggblockster.
+This game is unnofficial; so, it's not made by, endorsed or affiliated with Nintendo, the Pokémon Company, or GameFreak.
+All assets belong to their respective owners.
+
+That being said, enjoy the game!
+(please don't cheat)
+
+    - ggblockster
+    
+Thanks for playing PokéGuessr!
+
+My Website:`, "font-family: sans-serif; font-size: 14px;");
+console.log(`
+%chttps://ggblockster.pages.dev/`,
+"font-family: sans-serif; font-size: 14px; background-color: #213041; border-radius: 4px; padding: 16px; color: white; cursor: pointer; text-decoration: none;");
